eb362731614fabfdf20705ce662f5c9ee451f4d7
James Halliday <mail@substack.net>
2012-06-20 05:53:52 -0700

    refactored example shows how I've got weak maps backwards

--- example/weak.js
+++ example/weak.js
@@ -5,19 +5,31 @@ var s = (function () {
     var cons = {
         x : function (f, g) {
             setTimeout(function () { f(5) }, 2000);
-            setTimeout(function () { g(6) }, 2000);
+            setTimeout(function () { g(6) }, 4000);
         },
         y : 555
     };
-    return proto(cons, function (fn, id) {
-        weak(fn, function () { console.log('s.cull:' + id) });
-        return fn;
+    return proto(cons, function (cb, id) {
+        var ref = weak(cb, function () {
+            console.log('s.cull(' + id + ')')
+            s.cull(id);
+        });
+        return function () {
+            var f = weak.get(ref);
+            if (f) f.apply(this, arguments);
+        };
     });
 })();
 
-var c = proto({}, function wrapc (fn, id) {
-    weak(fn, function () { console.log('c.cull:' + id) });
-    return fn;
+var c = proto({}, function (cb, id) {
+    var ref = weak(cb, function () {
+        console.log('c.cull(' + id + ')')
+        c.cull(id);
+    });
+    return function () {
+        var f = weak.get(ref);
+        if (f) f.apply(this, arguments);
+    };
 });
 
 s.on('request', c.handle.bind(c));
@@ -32,9 +44,11 @@ c.on('remote', function (remote) {
 s.start();
 c.start();
 
+/*
 setTimeout(function () {
     // switch on the garbage disposal to full blast:
     var xs = [];
     for (var i = 0; i < 1000 * 1000; i++) xs.push(function () {});
     xs = [];
 }, 2500);
+*/
